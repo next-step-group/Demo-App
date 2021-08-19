960a1bf071b31c42da08389885077f68d7df390b
Gal Horovits <galhorovits@Gals-MBP.lan>
2021-08-19 12:10:47 -0400

    fixed writing file to metrics.json

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,26 +1,33 @@
-import nc from 'next-connect';
-import fs from 'fs';
+import nc from "next-connect";
+import fs from "fs";
 
-const handler = nc()
-  .post((req, res) => {
-    const json = JSON.parse(req.body)
-    const { name, startTime, value } = json;
-    const data = {
-      name,
-      startTime,
-      value
-    }
-    fs.appendFileSync('metrics.json', JSON.stringify(data) + ", ", (err) => {
-        console.log('err');
+const handler = nc().post((req, res) => {
+  const json = JSON.parse(req.body);
+  const { name, value } = json;
+
+  if (!fs.existsSync("metrics.json")) {
+    const newObj = { metrics: [{}] };
+    fs.writeFileSync("metrics.json", JSON.stringify(newObj, null, 4));
+  }
+
+  const results = JSON.parse(
+    fs.readFileSync("metrics.json", "utf-8", (err, data) => {
+      console.log(data);
     })
-    res.status(200).json({test: 'test worked'})
-  });
+  );
+  results.metrics[0][name] = value; //.toFixed(2);
 
-  export default handler;
+  fs.writeFileSync("metrics.json", JSON.stringify(results, null, 4));
 
+  // fs.appendFileSync("metrics.json", JSON.stringify(data), (err) => {
+  //   console.log("err");
+  // });
+  res.status(200).json({ test: "test worked" });
+});
 
+export default handler;
 
-  // {
+// {
 // 	"metrics": [
 // 		{
 // 			"FCP": "1.2s",
@@ -29,3 +36,16 @@ const handler = nc()
 // 		}
 // 	]
 // }
+// JSON.stringify(data) + ", ";
+
+//Name: FCP
+//value: num
+
+//name: CLS
+//value: num
+
+//name: TTFB
+//value: num
+
+//name: LCP
+//value: num
