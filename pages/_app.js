7b80ce672219e5449f5bba562256d6dbb1e30c2b
Gal Horovits <galhorovits@Gals-MBP.lan>
2021-08-23 12:05:32 -0400

    added functionality of reportwebvitals and importMetrics from npm package draft

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,23 @@
-import '../styles/globals.css';
-import Layout from '../components/Layout';
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { reportWebVitals } from "next-step-npm";
 
+// export async function reportWebVitals(metric) {
+//   const body = JSON.stringify(metric);
+//   const url = '/api'; -----> this file holds handler function
 
-export async function reportWebVitals(metric) {
-  const body = JSON.stringify(metric);
-  const url = '/api';
+//   // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+//   console.log('BODY IS', body);
+//   await fetch(url, { body, method: 'POST', keepalive: true })
 
-  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-  console.log('BODY IS', body);
-  await fetch(url, { body, method: 'POST', keepalive: true })
-  
-}
-
+// }
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
-
-
-export default MyApp
+export { reportWebVitals };
+export default MyApp;
